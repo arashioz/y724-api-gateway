ac7f3a75ad330bd2dfdfef89537dabd80fdb9b81
Soheil <dev.callmeskylark@gmail.com>
2023-08-08 17:56:47 +0330

    minor changes and new register dto added

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -6,14 +6,30 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { LoginDto, LoginDtoRs } from './dto/login.dto';
-@ApiTags('auth')
-@Controller('auth')
+@ApiTags('auth-actor')
+@Controller('auth/actor')
 export class AuthController {
   @Post('login')
   @ApiBody({ type: LoginDto })
   @ApiAcceptedResponse()
   @ApiResponse({ type: LoginDtoRs })
   login(@Body() body: LoginDto): LoginDtoRs {
+    let userData = {
+      role: 'expert',
+      userId: '3812hg31239cs',
+      email: 'userData.email',
+      clientKey: 'userData.clientKey',
+      token: 'userData.token',
+      refreshToken: 'userData.refreshToken',
+    };
+    return new LoginDtoRs(userData);
+  }
+
+  @Post('register')
+  @ApiBody({ type: LoginDto })
+  @ApiAcceptedResponse()
+  @ApiResponse({ type: LoginDtoRs })
+  register(@Body() body: LoginDto): LoginDtoRs {
     let userData = {
       role: 'expert',
       userId: '3812hg31239cs',
